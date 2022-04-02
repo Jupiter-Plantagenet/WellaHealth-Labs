49a163a5e5ef3b7fd001bcac3b55ad696faa9125
Jupiter-Plantagenet <61136165+Jupiter-Plantagenet@users.noreply.github.com>
2022-04-02 09:32:29 +0100

    Add files via upload

---
+++ LabOne.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello George theStormGod, I am in the Backend Engineering Track of Wella Schools");
+Console.WriteLine("As an Engineering student, I see learning Backend Enginering as a perfect skill to add to my repertoire,\n as a means of increasing my employability in a fast paced, technology driven world");
+
---
+++ LabTwo.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+class LabTwo
+{
+    public static void Main(string[] args)
+    {
+        //Declaration of ten different variables with ten different data types
+        string nameOfContact;
+        int numOfChildren;
+        char alphaNumeric;
+        float weight;
+        long milliseconds;
+        bool isVisible;
+        short numOfTrains;
+        byte totalSpent;
+        ushort temperature;
+        double distanceFromTheSun;
+
+        //Implicit type Conversion
+        numOfChildren = 7;
+        milliseconds = numOfChildren;
+        Console.WriteLine(milliseconds);
+
+        //Explicit type Conversion
+        distanceFromTheSun = 149.5e6d;
+        milliseconds = (long)distanceFromTheSun;
+        Console.WriteLine(milliseconds);
+    }
+}
